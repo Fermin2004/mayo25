b059db93d0fe934cdb9ca41b6c1ddd5940d76ffe
Fermin2004 <ferminjavierarroalvarez@gmail.com>
2025-05-20 12:01:42 +0000

    primer commit antes de comentar

---
+++ ej.php
@@ -0,0 +1,21 @@
+/*
+*@author fermin arro
+*/
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Ejercicio</title>
+	</head>
+	<body>
+		<h1>Ejercicio</h1>
+/*
+*Esto es una funcion que saluda al usuario 5 veces en php
+*/
+<?php
+for ($i = 1; $i <= 5; $i++) {
+	echo "\t\tHola mundo!<br>\n";
+}
+?>
+	</body>
+</html>
